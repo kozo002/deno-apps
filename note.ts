72524a7db418814e774a49ee362757e2fbc1036a
kozo yamagata <tune002@gmail.com>
2021-04-23 10:08:32 +0900

    Adds a link to note

--- note.ts
+++ note.ts
@@ -1,5 +1,8 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("note", {
+  const body = `
+    <a href="https://note.com/info/n/nea1b96233fbf" target="_blank">https://note.com/info/n/nea1b96233fbf</a>
+  `
+  const response = new Response(body, {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
