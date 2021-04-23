a2cc5d1dd475a67440fe0e3a836bcac6bc342269
kozo yamagata <516249+kozo002@users.noreply.github.com>
2021-04-23 15:12:23 +0900

    Update note.ts

--- note.ts
+++ note.ts
@@ -1,6 +1,8 @@
 addEventListener("fetch", (event) => {
   const body = `
     <a href="https://note.com/info/n/nea1b96233fbf" target="_blank">https://note.com/info/n/nea1b96233fbf</a>
+    <br />
+    <a href="http://localhost:3101">http://localhost:3101</a>
   `
   const response = new Response(body, {
     headers: { "content-type": "text/html" },
